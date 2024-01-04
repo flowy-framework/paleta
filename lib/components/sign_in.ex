15b72be71929cc663b1e1522a5816dbd4cc866ad
Emiliano Jankowski <ejankowski@gmail.com>
2024-01-04 14:41:51 +0100

    Making sign in logo css an attrs

--- lib/components/sign_in.ex
+++ lib/components/sign_in.ex
@@ -6,13 +6,14 @@ defmodule Paleta.Components.SignIn do
   attr(:oauth_path, :string, required: true)
   attr(:footer, :boolean, default: true)
   attr(:logo, :string, default: "/images/logo-300.png")
+  attr(:logo_css, :string, default: "mx-auto h-16 w-24")
 
   def sign_in(assigns) do
     ~H"""
     <main class="grid w-full grow grid-cols-1 place-items-center">
       <div class="w-full max-w-[26rem] p-4 sm:px-5">
         <div class="text-center">
-          <img class="mx-auto h-16 w-24" src={@logo} alt="logo" />
+          <img class={@logo_css} src={@logo} alt="logo" />
           <div class="mt-4">
             <h2 class="text-2xl font-semibold text-slate-600 dark:text-navy-100">
               <%= @title %>
