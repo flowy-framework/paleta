a38325968a95794886f420d9da29e34c0193ee6d
Memo <guillermo@dinkuminteractive.com>
2024-02-21 17:13:30 -0300

    Improve components

--- lib/components/body.ex
+++ lib/components/body.ex
@@ -2,10 +2,15 @@ defmodule Paleta.Components.Body do
   use Phoenix.Component
 
   slot(:inner_block, doc: "Body content", required: true)
+  attr(:left_sidebar_open, :boolean, default: false)
 
   def body(assigns) do
     ~H"""
-    <body x-data class="is-header-blur" x-bind="$store.global.documentBody">
+    <body
+      x-data
+      class={"is-header-blur" <> (@left_sidebar_open && " is-sidebar-open" || "")}
+      x-bind="$store.global.documentBody"
+    >
       <%= render_slot(@inner_block) %>
     </body>
     """
--- lib/components/sidebar/panel.ex
+++ lib/components/sidebar/panel.ex
@@ -2,27 +2,24 @@ defmodule Paleta.Components.Sidebar.Panel do
   use Phoenix.Component
 
   slot(:inner_block, required: true)
+  attr(:title, :string, default: "Home")
 
   def sidebar_panel(assigns) do
-    assigns =
-      assigns
-      |> assign_new(:title, fn -> "Home" end)
-
     ~H"""
     <div class="sidebar-panel">
       <div class="flex h-full grow flex-col bg-white pl-[var(--main-sidebar-width)] dark:bg-navy-750">
         <!-- Sidebar Panel Header -->
-        <div class="flex h-18 w-full items-center justify-between pl-4 pr-1">
+        <div class="flex items-center justify-between w-full pl-4 pr-1 h-18">
           <p class="text-base tracking-wider text-slate-800 dark:text-navy-100">
             <%= @title %>
           </p>
           <button
             @click="$store.global.isSidebarExpanded = false"
-            class="btn h-7 w-7 rounded-full p-0 text-primary hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:text-accent-light/80 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25 xl:hidden"
+            class="p-0 rounded-full btn h-7 w-7 text-primary hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:text-accent-light/80 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25 xl:hidden"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              class="w-6 h-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -42,7 +39,7 @@ defmodule Paleta.Components.Sidebar.Panel do
           class="h-[calc(100%-4.5rem)] overflow-x-hidden pb-6"
           x-init="$el._x_simplebar = new SimpleBar($el);"
         >
-          <ul class="flex flex-1 flex-col px-4 font-inter">
+          <ul class="flex flex-col flex-1 px-4 font-inter">
             <%= render_slot(@inner_block) %>
           </ul>
         </div>
