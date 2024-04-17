f46391900e99679556dce378fe41d9b4859ca736
Memo <guillermo@dinkuminteractive.com>
2024-04-17 19:19:31 -0300

    Display user avatar in the sidebar if one was set

--- lib/components/sidebar_profile.ex
+++ lib/components/sidebar_profile.ex
@@ -16,17 +16,25 @@ defmodule Paleta.Components.SidebarProfile do
       @click.outside="if(isShowPopper) isShowPopper = false"
       class="flex"
     >
-      <button @click="isShowPopper = !isShowPopper" x-ref="popperRef" class="avatar h-12 w-12">
-        <img class="rounded-full" src="/images/default-user-avatar.jpg" alt="avatar" />
+      <button @click="isShowPopper = !isShowPopper" x-ref="popperRef" class="w-12 h-12 avatar">
+        <img
+          class="rounded-full"
+          src={@user.avatar_url || "/images/default-user-avatar.jpg"}
+          alt="avatar"
+        />
         <span class="absolute right-0 h-3.5 w-3.5 rounded-full border-2 border-white bg-success dark:border-navy-700">
         </span>
       </button>
 
-      <div x-bind:class="isShowPopper && 'show'" class="popper-root fixed" x-ref="popperRoot">
-        <div class="popper-box w-64 rounded-lg border border-slate-150 bg-white shadow-soft dark:border-navy-600 dark:bg-navy-700">
-          <div class="flex items-center space-x-4 rounded-t-lg bg-slate-100 py-5 px-4 dark:bg-navy-800">
+      <div x-bind:class="isShowPopper && 'show'" class="fixed popper-root" x-ref="popperRoot">
+        <div class="w-64 bg-white border rounded-lg popper-box border-slate-150 shadow-soft dark:border-navy-600 dark:bg-navy-700">
+          <div class="flex items-center px-4 py-5 space-x-4 rounded-t-lg bg-slate-100 dark:bg-navy-800">
             <div class="avatar h-14 w-14">
-              <img class="rounded-full" src="/images/default-user-avatar.jpg" alt="avatar" />
+              <img
+                class="rounded-full"
+                src={@user.avatar_url || "/images/default-user-avatar.jpg"}
+                alt="avatar"
+              />
             </div>
             <div>
               <a
@@ -35,18 +43,18 @@ defmodule Paleta.Components.SidebarProfile do
               >
                 <%= @full_name %>
               </a>
-              <p class="break-all text-xs text-slate-400 dark:text-navy-300">
+              <p class="text-xs break-all text-slate-400 dark:text-navy-300">
                 <%= @user.email %>
               </p>
             </div>
           </div>
           <div class="flex flex-col pt-2 pb-5">
-            <div class="mt-3 px-4">
+            <div class="px-4 mt-3">
               <.link
                 href={@profile_path}
-                class="group flex items-center space-x-3 py-2 px-4 tracking-wide outline-none transition-all hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600 rounded-md mb-2"
+                class="flex items-center px-4 py-2 mb-2 space-x-3 tracking-wide transition-all rounded-md outline-none group hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-navy-600 dark:focus:bg-navy-600"
               >
-                <div class="flex h-8 w-8 items-center justify-center rounded-lg bg-warning text-white">
+                <div class="flex items-center justify-center w-8 h-8 text-white rounded-lg bg-warning">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     class="h-4.5 w-4.5"
@@ -65,7 +73,7 @@ defmodule Paleta.Components.SidebarProfile do
                 </div>
 
                 <div>
-                  <h2 class="font-medium text-slate-700 transition-colors group-hover:text-primary group-focus:text-primary dark:text-navy-100 dark:group-hover:text-accent-light dark:group-focus:text-accent-light">
+                  <h2 class="font-medium transition-colors text-slate-700 group-hover:text-primary group-focus:text-primary dark:text-navy-100 dark:group-hover:text-accent-light dark:group-focus:text-accent-light">
                     Profile
                   </h2>
                   <div class="text-xs text-slate-400 line-clamp-1 dark:text-navy-300">
@@ -77,11 +85,11 @@ defmodule Paleta.Components.SidebarProfile do
               <.link
                 href={@logout_path}
                 method="delete"
-                class="btn h-9 w-full space-x-2 bg-primary text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90"
+                class="w-full space-x-2 text-white btn h-9 bg-primary hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  class="w-5 h-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
