6a4126d09abbe110437cd0a40fa8683ef60fd02f
mustela <ejankowski@gmail.com>
2024-04-29 16:31:26 +0200

    h-32

--- lib/components/dummy.ex
+++ lib/components/dummy.ex
@@ -9,7 +9,7 @@ defmodule Paleta.Components.Dummy do
 
   def dummy(assigns) do
     ~H"""
-    <div class="size-10 size-16 size-20 size-28"></div>
+    <div class="h-32 size-10 size-16 size-20 size-28"></div>
     """
   end
 end
