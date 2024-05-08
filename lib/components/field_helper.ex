133a43512575df182c5d522597499dd0a6770c93
mustela <ejankowski@gmail.com>
2024-05-08 18:07:44 +0200

    improving values

--- lib/components/field_helper.ex
+++ lib/components/field_helper.ex
@@ -6,7 +6,7 @@ defmodule Paleta.Components.FieldHelper do
       assigns
       |> assign(:id, field.id)
       |> assign(:name, field.name)
-      |> assign(:value, field.value)
+      |> assign(:value, parse_value(field.value))
       |> assign(:errors, field.errors)
     end
   end
@@ -15,7 +15,19 @@ defmodule Paleta.Components.FieldHelper do
     assigns
     |> assign(:id, field.id)
     |> assign(:name, field.name)
-    |> assign(:value, field.value)
+    |> assign(:value, parse_value(field.value))
     |> assign(:errors, field.errors)
   end
+
+  # This is mainly used for the advance select component,
+  # but it can be used for other components as well. We basically
+  # can't have atoms as values
+  defp parse_value(value) when is_atom(value), do: Atom.to_string(value)
+
+  defp parse_value(values) when is_list(values) do
+    values
+    |> Enum.map(&parse_value(&1))
+  end
+
+  defp parse_value(value), do: value
 end
