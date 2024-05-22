5d8fe4df6f32bc54f90546d5a68a03f662be9f08
Memo <guillermo@dinkuminteractive.com>
2024-05-22 01:25:12 -0300

    Fix avatar tests to use src attribute instead of img

--- test/components/avatars/avatar_test.exs
+++ test/components/avatars/avatar_test.exs
@@ -10,24 +10,24 @@ defmodule Paleta.Components.AvatarTest do
   describe "avatar/1" do
     @describetag :avatar
     test "avatar will be rendered properly with default type", %{assigns: assigns} do
-      h = ~H(<.avatar img="/img/avatar.jpg" />)
+      h = ~H(<.avatar src="/img/avatar.jpg" />)
 
       assert rendered_to_string(h) ==
-               "<div class=\"avatar h-8 w-8 \">\n  <img class=\"rounded-full\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
+               "<div class=\"avatar size-8 \">\n  <img class=\"rounded-full\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
     end
 
     test "avatar will be rendered properly with square type", %{assigns: assigns} do
-      h = ~H(<.avatar type={:square} img="/img/avatar.jpg" />)
+      h = ~H(<.avatar type={:square} src="/img/avatar.jpg" />)
 
       assert rendered_to_string(h) ==
-               "<div class=\"avatar h-8 w-8 \">\n  <img class=\"rounded-lg\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
+               "<div class=\"avatar size-8 \">\n  <img class=\"rounded-lg\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
     end
 
     test "avatar will be rendered properly with squircle type", %{assigns: assigns} do
-      h = ~H(<.avatar type={:squircle} img="/img/avatar.jpg" size="10" />)
+      h = ~H(<.avatar type={:squircle} src="/img/avatar.jpg" size="10" />)
 
       assert rendered_to_string(h) ==
-               "<div class=\"avatar h-10 w-10 \">\n  <img class=\"mask is-squircle\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
+               "<div class=\"avatar size-10 \">\n  <img class=\"mask is-squircle\" src=\"/img/avatar.jpg\" alt=\"avatar\">\n</div>"
     end
   end
 end
