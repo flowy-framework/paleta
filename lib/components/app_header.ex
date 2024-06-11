b701b7f263036fac2fb47b6115fd48e1b6dea0e2
Memo <gtenaschuk@gmail.com>
2024-06-11 17:28:43 -0300

    Show search icon in mobile only when search is enabled

--- lib/components/app_header.ex
+++ lib/components/app_header.ex
@@ -36,28 +36,28 @@ defmodule Paleta.Components.AppHeader do
         </div>
         <!-- Right: Header buttons -->
         <div class="-mr-1.5 flex items-center space-x-2 md:ml-auto">
-          <!-- Mobile Search Toggle -->
-          <button
-            @click="$store.global.isSearchbarActive = !$store.global.isSearchbarActive"
-            class="w-8 h-8 p-0 rounded-full btn hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25 sm:hidden"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5.5 w-5.5 text-slate-500 dark:text-navy-100"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="1.5"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <!-- Main Searchbar -->
           <%= if @search do %>
+            <!-- Mobile Search Toggle -->
+            <button
+              @click="$store.global.isSearchbarActive = !$store.global.isSearchbarActive"
+              class="w-8 h-8 p-0 rounded-full btn hover:bg-slate-300/20 focus:bg-slate-300/20 active:bg-slate-300/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25 sm:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5.5 w-5.5 text-slate-500 dark:text-navy-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="1.5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+            <!-- Main Searchbar -->
             <.live_component
               module={Paleta.Components.Live.GlobalSearch}
               id="global-search"
