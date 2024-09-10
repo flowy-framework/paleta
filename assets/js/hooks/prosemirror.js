6a62f7ace8b0f9ad56fd469e4ec8bd4109bbe7c4
Memo <gtenaschuk@gmail.com>
2024-09-10 09:37:52 -0300

    Upgrade prosemirror and delete autofocus action

--- assets/js/hooks/prosemirror.js
+++ assets/js/hooks/prosemirror.js
@@ -9,8 +9,10 @@ import {
 import { exampleSetup } from "prosemirror-example-setup";
 
 export let ProsemirrorEditor = {
-  hidden_id() { return this.el.dataset.hidden_id},
-  
+  hidden_id() {
+    return this.el.dataset.hidden_id;
+  },
+
   mounted() {
     class ProseMirrorView {
       constructor(target, content) {
@@ -35,32 +37,29 @@ export let ProsemirrorEditor = {
         let newState = EditorState.create({
           doc: defaultMarkdownParser.parse(value),
           plugins: exampleSetup({ schema }),
-        })
+        });
         this.view.updateState(newState);
       }
     }
 
     let place = document.querySelector("#prosemirror-editor");
     let hidden_id = this.hidden_id();
-    
+
     let editorView = new ProseMirrorView(
       place,
       document.getElementById(hidden_id).value
     );
-    
-    this.handleEvent(`reset-${this.el.id}`, data => {
+
+    this.handleEvent(`reset-${this.el.id}`, (data) => {
       editorView.reset(data.value);
-    })
+    });
 
     editorView.view.setProps({
       dispatchTransaction(transaction) {
         let newState = editorView.view.state.apply(transaction);
         editorView.view.updateState(newState);
-        document.getElementById(hidden_id).value =
-          editorView.content;
+        document.getElementById(hidden_id).value = editorView.content;
       },
     });
-
-    editorView.focus();
   },
 };
